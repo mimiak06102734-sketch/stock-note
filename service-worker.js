859efa5e2050075c9d0f9440593a8b5a9118409d
mimiak06102734-sketch <mimiak.06102734@gmail.com>
2026-09-06 14:57:39 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const APP_SHELL = [
   './style.css',
   './script.js',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+　'./icon-512.png'
 ];
 
 self.addEventListener('install', event => {
